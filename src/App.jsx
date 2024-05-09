e209990d0ebf6c2437f165834087ef555c670ac5
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-09 14:42:33 +0900

    자바스크립트 데이터 타입 undefined, string, number, boolean, object, function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
 function App() {
-  return <></>;
+  //javascript data type
+  let a; //값이 없으면 undefined
+  console.log("a", typeof a);
+
+  let b = "문자열타입"; //string
+  console.log("b", typeof b);
+
+  let c = 3; //number
+  console.log("c", typeof c);
+
+  let d = 3.14; //number
+  console.log("d", typeof d);
+
+  let e = true;
+  console.log("e", typeof e);
+
+  let f = {}; //object
+  console.log("f", typeof f);
+
+  let g = function () {
+    console.log("g", typeof g);
+  };
+
+  return null;
 }
 
 export default App;
