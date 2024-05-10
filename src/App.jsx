a30d990de95307313b85c334e38bbf1108714ef9
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-10 14:22:13 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
 
-function MyComp(props) {
-  return (
-    <div>
-      <p>{props.name}</p>
-      <p>{props.age}</p>
-      <p>{props.address}</p>
-      <p>{props.city}</p>
-      <p>{props.country}</p>
-    </div>
-  );
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyComp name="son" age={33} address="seoul" />
-      <MyComp city="busan" country="USA" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
