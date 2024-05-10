45803280d599dd1dd36221e45a9f829cc122577d
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-10 16:01:15 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <button
-        onClick={() => console.lop("click")}
-        onMouseEnter={() => console.log("mouse enter")}
-        onMouseLeave={() => console.log("mouse leave")}
-      >
-        클릭1
-      </button>
-      <input type={"text"} onChange={() => console.log("change")} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
