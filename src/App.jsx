6b4b8d9ffcb43a423ec608121d80d5b6706a9da7
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-10 15:19:20 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 import React from "react";
 
-function MyComp(props) {
-  return <div></div>;
-}
-
 function App(props) {
-  //props의 이름은 lowerCamelCase로
-  //예약어를 쓰지 말 것
-  return (
-    <div>
-      <label htmlFor="input1"></label>
-      <input id="input1" type="text" />
-      <p className="error note">Lorem ipsum dolor.</p>
-      <MyComp number={3} pageNumber={3} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
