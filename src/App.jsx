d10a71bf70032f3d94fd864a1173feccfd54f02b
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-10 15:38:31 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import React from "react";
 
-function MyComp({ name, age, someProp = false, otherProp }) {
-  console.log("name", name);
-  console.log("age", age);
-  console.log("someProp", someProp);
-  console.log("otherProp", otherProp);
-  return <div></div>;
-}
 function App(props) {
-  return (
-    <div>
-      <MyComp name={"son"} age={30} someProp otherProp={true} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
