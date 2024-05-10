0072d2cd81f42bf15c5d070de438f5f263d3aa36
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-10 15:44:18 +0900

    이벤트 props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <button
+        onClick={() => console.lop("click")}
+        onMouseEnter={() => console.log("mouse enter")}
+        onMouseLeave={() => console.log("mouse leave")}
+      >
+        클릭1
+      </button>
+      <input type={"text"} onChange={() => console.log("change")} />
+    </div>
+  );
 }
 
 export default App;
