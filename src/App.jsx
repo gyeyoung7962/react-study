63ee61c36c4f4d269dea80d91e212e4331b946da
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-10 16:14:24 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 import React from "react";
 
-function MyComp({ color }) {
-  return (
-    <div style={{color:{color}}>
-      <h1>lorem</h1>
-      <p>Lorem ipsum dolor.</p>
-    </div>
-  );
-}
 function App(props) {
-  return (
-    <div>
-      <MyComp color={"red"} />
-      <MyComp color={"blue"} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
