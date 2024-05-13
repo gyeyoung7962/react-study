5af293d3aef6402609670abc7fb2cfb694034ee7
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-13 09:48:31 +0900

    import, export

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+//import : 다른 파일에 있는 변수를 현재 파일에서 사용
+import { value1, value2, value3, value4 } from "./MyValues.jsx";
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      {value1},{value2},{value3}, {value4.team}
+    </div>
+  );
 }
 
 export default App;
