328ab8c8cb7751989d0273aa73b55322419e3939
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-13 12:03:05 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import React, { useState } from "react";
-
-function MyComp() {
-  const [val, setVal] = useState(0);
-  console.log("다시 그려짐");
-  return (
-    //setVal(0)으로 하면 기존값과 같은 상태라 업데이트가 되지않는다
-    <div>
-      <button onClick={() => setVal(val + 1)}>클릭 {val}</button>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <MyComp />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
