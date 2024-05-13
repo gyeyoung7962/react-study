2cbc36915fe56785cca1052f1c337def1f9b22a3
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-13 11:11:58 +0900

    default export

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+//default export는 아무 이름으로 import할 수 있음
+import val, { v1 } from "./MyValues.jsx";
+import someVal from "./MyComps.jsx";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <div>
+          {v1},{val}
+        </div>
+        <div>{someVal}</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
