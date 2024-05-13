e2895cd226ce0fd40a1bdfb6e1d4c31be850924e
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-13 10:34:43 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 import React from "react";
-import { ArrowComp, MyComp, YourComp } from "./MyComps.jsx";
 
 function App(props) {
-  return (
-    <div>
-      <div>
-        <MyComp />
-      </div>
-      <div>
-        <YourComp />
-      </div>
-      <div>
-        <ArrowComp />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
