b0ede714b2d66bc5cfae69ccfd0b07bdb7f483bd
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-13 09:32:00 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,7 @@
 import React from "react";
 
-function MyComp({ name, age, children }) {
-  console.log(children);
-  return (
-    <div>
-      {name},{age}, {children}
-    </div>
-  );
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyComp name={"son"} age={33}>
-        {/*content는 children prop*/}
-        <p>Lorem ipsum dolor.</p>
-      </MyComp>
-
-      <MyComp name={"lee"} age={22}>
-        <ul>
-          <li>Lorem.</li>
-          <li>Quo?</li>
-        </ul>
-      </MyComp>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
