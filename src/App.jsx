107490bf6e7295f65f0f3a4df8999cd12ae362a5
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-13 11:42:03 +0900

    상태가 변경될 때 컴포넌트(함수)가 다시 실행

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyComp() {
+  const [val1, setVal1] = useState(0);
+  let val2 = 0;
+
+  //상태가 변경될때만 호출됨
+  console.log("다시 호출됨");
+  return (
+    <div>
+      <button onClick={() => setVal1(val1 + 1)}>val {val1}</button>
+      <button onClick={() => val2++}>val2 {val2}</button>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp />
+    </div>
+  );
 }
 
 export default App;
