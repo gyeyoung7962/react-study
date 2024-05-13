7562823fe87d913c38f4670a33b17eaf08ede413
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-13 11:43:58 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
-import React, { useState } from "react";
-
-function MyComp() {
-  const [val1, setVal1] = useState(0);
-  let val2 = 0;
-
-  //상태가 변경될때만 호출됨
-  console.log("다시 호출됨");
-  return (
-    <div>
-      <button onClick={() => setVal1(val1 + 1)}>val {val1}</button>
-      <button onClick={() => val2++}>val2 {val2}</button>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <MyComp />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
