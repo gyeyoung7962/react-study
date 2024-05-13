7065b70ad7c974dad239ba189cf121e5724b849b
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-13 14:02:41 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,7 @@
-import React, { useState } from "react";
-
-function MyComp() {
-  const [val1, setVal1] = useState(0);
-  const [val2, setVal2] = useState(0);
-
-  console.log("다시 그려짐");
-  return (
-    <div>
-      <div>{val1}</div>
-      <div>
-        <button onClick={() => setVal1(val1 + 1)}>click</button>
-      </div>
-      <div>{val2}</div>
-      <div>
-        <button onClick={() => setVal2(val2 + 1)}>click2</button>
-      </div>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <MyComp />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
