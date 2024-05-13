bf8fa1aad8aa379c2e2b4cf2f9a820bb0b112f72
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-13 09:23:30 +0900

    children prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+function MyComp({ name, age, children }) {
+  console.log(children);
+  return (
+    <div>
+      {name},{age}, {children}
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp name={"son"} age={33}>
+        {/*content는 children prop*/}
+        <p>Lorem ipsum dolor.</p>
+      </MyComp>
+
+      <MyComp name={"lee"} age={22}>
+        <ul>
+          <li>Lorem.</li>
+          <li>Quo?</li>
+        </ul>
+      </MyComp>
+    </div>
+  );
 }
 
 export default App;
