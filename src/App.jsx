a5ceb746bfc0e6b514ad929676ba80dc01f9e3a8
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-13 11:47:14 +0900

    상태가 바뀔 때만 다시 그림

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyComp() {
+  const [val, setVal] = useState(0);
+  console.log("다시 그려짐");
+  return (
+    //setVal(0)으로 하면 기존값과 같은 상태라 업데이트가 되지않는다
+    <div>
+      <button onClick={() => setVal(val + 1)}>클릭 {val}</button>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp />
+    </div>
+  );
 }
 
 export default App;
