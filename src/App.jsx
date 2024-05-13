49409235b1c73d9f5a39e7223e26935bf5d538b5
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-13 15:35:38 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [text, setText] = useState("");
-  const [list, setList] = useState([]);
-
-  function handleClick() {
-    const [...nextList] = list; //새로운 객체에 값 복사
-    nextList.push(text); //새로운 객체에 입력한 값을 넣고
-    setList(nextList); // list에 그 객체를 셋하기
-    setText("");
-  }
-
-  return (
-    <div>
-      <input
-        value={text}
-        type={"text"}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button onClick={handleClick}>추가</button>
-      <ul>
-        {list.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
