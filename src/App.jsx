395588e75b382d0a6db354abfe2894b3a9b44daf
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-13 10:26:32 +0900

    react Component의 import, export

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-
-//import : 다른 파일에 있는 변수를 현재 파일에서 사용
-//다른 파일의 ㅂ같은 변수를 import 할때 as로 다른 별칭을 줄 수 있음
-import { value1, value2, value3 as v3, value4 } from "./MyValues.jsx";
-import { myAddress, myStr, value3 as vv3 } from "./MyComps.jsx";
+import { action1, action2, action3, MyComp as YourComp } from "./MyComps.jsx";
+import { HerComp } from "./HerComp.jsx";
 
 function App(props) {
+  action1();
+  action2();
+  action3();
+  YourComp();
   return (
     <div>
-      {value1},{value2},{v3}, {value4.team}
-      <div>
-        {myStr},{myAddress},{vv3}
-      </div>
+      <YourComp />
+      <HerComp />
     </div>
   );
 }
