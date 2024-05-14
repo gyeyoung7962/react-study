989abe5f78195a43172c1e9e848e592f07d7a7c8
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-14 12:18:43 +0900

    props drilling 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyInput({ text, onChange }) {
+  return (
+    <div>
+      <input type="text" onChange={onChange} />
+      <p>{text}</p>
+    </div>
+  );
+}
+
+function MyText({ text }) {
+  return (
+    <div>
+      <p>{text}</p>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  const [text, setText] = useState("");
+
+  function handleUpdateText(e) {
+    setText(e.target.value);
+  }
+  return (
+    <div>
+      <MyInput text={text} onChange={handleUpdateText} />
+      <MyText text={text} />
+    </div>
+  );
 }
 
 export default App;
