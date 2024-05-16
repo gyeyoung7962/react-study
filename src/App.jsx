b38308c141fa7263a1e41ef0697e7d2ad6b37271
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-16 12:34:27 +0900

    js객체가 json으로 직렬화됨 이 때 각 데이터타입이 사용됨

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,28 @@ function App(props) {
     axios.post("/api/main42/sub5", data);
   }
 
+  function handleClick2() {
+    axios.post("/api/main42/sub6", {
+      name: "son",
+      age: 33,
+    });
+  }
+
+  function handleClick3() {
+    axios.post("/api/main42/sub7", {
+      name: "옥수수",
+      age: 10,
+    });
+  }
+
   //문자열, 수, 불리언, 객체, 배열, null
   return (
     <div>
       <button onClick={handleClick1}>json 데이터와 요청</button>
+      <br />
+      <button onClick={handleClick2}>json 데이터를 요청2</button>
+      <br />
+      <button onClick={handleClick3}>json 데이터와 요청3</button>
     </div>
   );
 }
