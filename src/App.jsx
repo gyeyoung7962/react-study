298d323d4ad898a9a6dc5bc8893e3dd7132afb14
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-16 09:36:21 +0900

    /api/someurl의 get,post ajax요청 버튼 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handleClickButton1() {
+    //ajax get요청
+    axios.get("/api/someurl");
+  }
+
+  function handleClickButton2() {
+    axios.post("/api/someurl");
+  }
+
+  return (
+    <div>
+      <button onClick={handleClickButton1}>get요청</button>
+      <button onClick={handleClickButton2}>post요청</button>
+    </div>
+  );
 }
 
 export default App;
