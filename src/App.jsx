f76a565f970c01df8943e778ae908d6f516a10af
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-16 15:07:15 +0900

    JSON 응답 파싱 하기4

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,10 @@ function App(props) {
     axios.get("/api/main43/sub5").then((resp) => console.log(resp.data));
   }
 
+  function handleClick6() {
+    axios.get("/api/main43/sub6").then((resp) => console.log(resp.data));
+  }
+
   return (
     <div>
       <button onClick={handleClick1}>응답 받기</button>
@@ -37,6 +41,8 @@ function App(props) {
       <button onClick={handleClick4}>응답 받기4</button>
       <br />
       <button onClick={handleClick5}>응답 받기5</button>
+      <br />
+      <button onClick={handleClick6}>응답 받기6</button>
     </div>
   );
 }
