1310d67dcf54b314bbc9901795996ccfb9f65822
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-16 10:39:51 +0900

    /api/someurl4의 get,post ajax요청 버튼 작성

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,30 @@ function App(props) {
     axios.get(`/api/someurl3?${param}`);
   }
 
+  function handleClickButton6() {
+    const param = new URLSearchParams();
+    param.append("id", 5);
+    param.append("city", "서울");
+    param.append("country", "한국");
+    axios.post("/api/someurl3", param);
+  }
+
+  function handleClickButton7() {
+    const param = new URLSearchParams();
+    param.append("id", 1);
+    param.append("name", "라이언");
+    param.append("goods", "큐티라이언");
+    axios.get(`/api/someurl4?${param}`);
+  }
+
+  function handleClickButton8() {
+    const param = new URLSearchParams();
+    param.append("id", 1);
+    param.append("name", "라이언");
+    param.append("goods", "큐티라이언");
+    axios.post(`/api/someurl4?${param}`);
+  }
+
   return (
     <div>
       <button onClick={handleClickButton1}>get요청</button>
@@ -34,6 +58,10 @@ function App(props) {
       <button onClick={handleClickButton3}>get요청2</button>
       <button onClick={handleClickButton4}>post요청2</button>
       <button onClick={handleClickButton5}>get요청 with query string</button>
+      <button onClick={handleClickButton6}>post요청 with data</button>
+
+      <button onClick={handleClickButton7}>get요청 with data</button>
+      <button onClick={handleClickButton8}>post요청 with data</button>
     </div>
   );
 }
