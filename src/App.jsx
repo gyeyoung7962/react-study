12d7924dbfb46334acc19625e377dcac58575060
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-16 12:06:14 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,7 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
 function App(props) {
-  function handleClick1() {
-    const obj1 = {
-      name: "son",
-      age: 44,
-    };
-    // const json = JSON.stringify(obj1);  axios가 post보낼때 두번재 파라미터가 객체면 직렬화를 해주므로 안해줘도 상관없음
-    axios.post("/api/main42/sub1", obj1);
-  }
-
-  function handleClick2() {
-    const obj = {
-      name: "라이언자켓",
-      price: 500,
-    };
-
-    axios.post("/api/main42/sub2", obj);
-  }
-
-  const [name, setName] = useState("");
-  const [city, setCity] = useState("");
-
-  function handleClick3() {
-    // 같을경우 생략가능
-    // const data = {
-    //   name: name,
-    //   city: city,
-    // };
-
-    const data = {
-      name: name,
-      city: city,
-    };
-    axios.post("/api/main42/sub3", data);
-  }
-
-  const [product, setProduct] = useState("");
-  const [price, setPrice] = useState(0);
-
-  function handleClick4() {
-    const obj = {
-      product: product,
-      price: price,
-    };
-    axios.post("/api/main42/sub4", obj);
-  }
-
-  return (
-    <div>
-      <button onClick={handleClick1}>요청(json데이터 포함)</button>
-      <button onClick={handleClick2}>요청2(json데이터 포함)</button>
-
-      <div>
-        <input type="text" onChange={(e) => setName(e.target.value)} />
-        <br />
-        <input type="text" onChange={(e) => setCity(e.target.value)} />
-        <br />
-        <button onClick={handleClick3}>요청(input에 입력한 값)</button>
-      </div>
-
-      <div>
-        <input type="text" onChange={(e) => setProduct(e.target.value)} />
-        <br />
-        <input type="number" onChange={(e) => setPrice(e.target.value)} />
-        <button onClick={handleClick4}>요청2</button>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
