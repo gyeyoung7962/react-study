6397ca676ee72714d959364bb29d759b5e05155b
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-16 16:29:38 +0900

    응답 코드에 따라 다른일을 하도록 함2

--- src/App.jsx
+++ src/App.jsx
@@ -36,11 +36,81 @@ function App(props) {
       });
   }
 
+  function handleMove200() {
+    axios
+      .get("/api/main44/sub1")
+      .then((resp) => console.log(resp))
+      .catch((error) => {
+        if (error.response.status === 401) {
+          console.log("잘못된 경로입니다 401");
+        } else if (error.response.status === 403) {
+          console.log("잘못된 경로입니다 403");
+        }
+      });
+  }
+
+  function handleMove401() {
+    axios
+      .get("/api/main44/sub3")
+      .then((resp) => console.log(resp))
+      .catch((error) => {
+        if (error.response.status === 401) {
+          console.log("잘못된 경로입니다 401");
+        }
+      });
+  }
+
+  function handleMove403() {
+    axios
+      .get("/api/main44/sub4")
+      .then((resp) => console.log(resp))
+      .catch((error) => {
+        if (error.response.status === 403) {
+          console.log("잘못된 경로입니다 403");
+        }
+      });
+  }
+
+  function handle999() {
+    const r = Math.random();
+    let path = "";
+    if (r < 1 / 3) {
+      path = "/api/main43/sub1";
+    } else if (r < 2 / 3) {
+      path = "/api/main43/sub3";
+    } else {
+      path = "/api/main43/sub4";
+    }
+    axios
+      .get(path)
+      .then(() => {
+        console.log("잘 됨 ");
+      })
+      .catch((error) => {
+        const code = error.response.status;
+
+        switch (code) {
+          case 401:
+            console.log("401 응답 입니다. 로그인되지 않았습니다.");
+            break;
+          case 403:
+            console.log("403 응답 입니다. 권한이 없습니다.");
+            break;
+        }
+      });
+  }
+
   return (
     <div>
       <button onClick={handle200}>200응답</button>
       <button onClick={handle400}>400응답</button>
       <button onClick={handle500}>500응답</button>
+
+      <button onClick={handleMove200}>응답 코드에 따라 다른 일하기200</button>
+      <button onClick={handleMove401}>응답 코드에 따라 다른 일하기401</button>
+      <button onClick={handleMove403}>응답 코드에 따라 다른 일하기403</button>
+
+      <button onClick={handle999}>응답 코드에 따라 다른 일하기403</button>
     </div>
   );
 }
