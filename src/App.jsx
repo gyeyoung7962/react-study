e29a134e6f9bac94ead4ed3ecdad8cbca2be86fc
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-16 15:40:04 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import axios from "axios";
 
 function App(props) {
-  return (
-    <div>
-      <button onClick={() => axios.get("/api/main44/sub1")}>200 응답</button>
-      <button onClick={() => axios.get("/api/main44/sub2")}>400응답</button>
-      <button onClick={() => axios.get("/api/main44/sub3")}>401응답</button>
-      <button onClick={() => axios.get("/api/main44/sub4")}>403응답</button>
-      <button onClick={() => axios.get("/api/main44/sub5")}>404응답</button>
-      <button onClick={() => axios.get("/api/main44/sub6")}>500응답</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
