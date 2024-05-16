015326ae4f4d802e6c4c73cbb068d900ed7286d2
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-16 15:39:52 +0900

    response status message

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <button onClick={() => axios.get("/api/main44/sub1")}>200 응답</button>
+      <button onClick={() => axios.get("/api/main44/sub2")}>400응답</button>
+      <button onClick={() => axios.get("/api/main44/sub3")}>401응답</button>
+      <button onClick={() => axios.get("/api/main44/sub4")}>403응답</button>
+      <button onClick={() => axios.get("/api/main44/sub5")}>404응답</button>
+      <button onClick={() => axios.get("/api/main44/sub6")}>500응답</button>
+    </div>
+  );
 }
 
 export default App;
