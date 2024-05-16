20c3b79a25b10d4e2e5c9cdea1b62a1f54a1ddf2
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-16 14:21:50 +0900

    응답받고 화면에 출력

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 function App(props) {
@@ -7,9 +7,19 @@ function App(props) {
     axios.get("/api/main43/sub1").then((param) => console.log(param.data));
   }
 
+  const [result, setResult] = useState("");
+
+  function handleClick2() {
+    axios.get("/api/main43/sub2").then((param) => setResult(param.data));
+  }
+
   return (
     <div>
       <button onClick={handleClick1}>응답 받기</button>
+      <br />
+      <button onClick={handleClick2}>응답 받기2</button>
+      <br />
+      <p>{result}</p>
     </div>
   );
 }
