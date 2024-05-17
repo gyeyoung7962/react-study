e376ab9b13c177690c69de9da139ab38b8d8adab
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-17 15:02:32 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,7 @@
 import React from "react";
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-import { BoardAdd } from "./BoardAdd.jsx";
-
-function BoardEdit() {
-  return <div>게시물 수정 페이지</div>;
-}
-
-function BoardList() {
-  return <div>게시물 목록 페이지</div>;
-}
-
-//router 객체
-const router = createBrowserRouter([
-  {
-    path: "/add",
-    element: <BoardAdd />,
-  },
-  { path: "/edit", element: <BoardEdit /> },
-  { path: "/list", element: <BoardList /> },
-
-  {
-    path: "/board",
-    element: <Outlet />,
-    children: [
-      { path: "write", element: <div>게시물 작성하기</div> },
-      { path: "edit", element: <div>게시물 수정하기</div> },
-    ],
-  },
-]);
 
 function App(props) {
-  return <RouterProvider router={router} />;
+  return <div></div>;
 }
 
 export default App;
--- src/BoardAdd.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function BoardAdd() {
-  return <div>글작성 페이지</div>;
-}
