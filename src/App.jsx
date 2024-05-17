e8a103762f9c6e58ecf01646b50b08f7de7cb920
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-17 16:08:09 +0900

    게시물 홈/ 게시물 목록

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+
+function Root() {
+  return (
+    <div>
+      <div>
+        <Link to={"/"}>home</Link>
+        <Link to={"list"}>list</Link>
+      </div>
+      <hr />
+      <Outlet />
+    </div>
+  );
+}
+
+function BoardList() {
+  return (
+    <div>
+      <div>1번 게시물 보기</div>
+      <div>2번 게시물 보기</div>
+      <div>3번 게시물 보기</div>
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { index: true, element: <div>main page</div> },
+      {
+        path: "board",
+        element: <div>board page</div>,
+      },
+      {
+        path: "list",
+        element: <BoardList />,
+      },
+    ],
+  },
+]);
 
 function App(props) {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
