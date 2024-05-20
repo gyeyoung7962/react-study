5f0e5161631a62f4cccff53c4a4533da9cc26bca
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-20 16:24:09 +0900

    필요 없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,7 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
 function App(props) {
-  const [name, setName] = useState("");
-  const [file, setFile] = useState([]);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    axios.postForm("/api/main46/sub2", {
-      name: name,
-      file: file,
-    });
-  }
-
-  const fileNames = [];
-  for (let i = 0; i < file.length; i++) {
-    fileNames.push(<li>{file.item(i).name}</li>);
-  }
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        이름
-        <input
-          type={"text"}
-          name={"name"}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <br />
-        <input
-          type={"file"}
-          name={"file"}
-          onChange={(e) => setFile(e.target.files)}
-          multiple={true}
-        />
-        <br />
-        <input type={"submit"} />
-      </form>
-      <div>
-        <ul>{fileNames}</ul>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
