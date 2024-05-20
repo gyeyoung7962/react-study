b36e7dd8c9e443f6e91739270912effaa58e6688
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-20 11:43:07 +0900

    Chkra ui icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
+import { MoonIcon, PlusSquareIcon } from "@chakra-ui/icons";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <div>
+        <MoonIcon />
+      </div>
+      <div>
+        <Button leftIcon={<PlusSquareIcon />}>버튼</Button>
+      </div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
