dd32a31b16f5fded4732671dcb4e765e3f08fd09
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-20 12:26:23 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
-import { MoonIcon, PlusSquareIcon } from "@chakra-ui/icons";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <div>
-        <MoonIcon />
-      </div>
-      <div>
-        <Button leftIcon={<PlusSquareIcon />}>버튼</Button>
-      </div>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
