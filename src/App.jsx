7ccaaf64e8962164f18e9c1df28090b9c43a94f2
gyeyoung7962 <gaeyoung7962@naver.com>
2024-05-20 11:13:08 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,10 @@
 import React from "react";
-import {
-  Center,
-  ChakraProvider,
-  Checkbox,
-  FormControl,
-  FormLabel,
-  Radio,
-  RadioGroup,
-  Textarea,
-} from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <ChakraProvider>
-      <label htmlFor={"input1"}>이름</label>
-      <input id={"input1"} />
-      <hr />
-      <Center
-        w={{
-          base: "100%",
-          md: "50%",
-        }}
-      >
-        <FormControl>
-          <FormLabel>이름</FormLabel>
-          <input />
-        </FormControl>
-      </Center>
-      <hr />
-      <Checkbox>lorem</Checkbox>
-      <Checkbox>lorem1</Checkbox>
-      <Checkbox>lorem2</Checkbox>
-      <Checkbox>lorem3</Checkbox>
-      <hr />
-      <RadioGroup>
-        <Radio value={1}>1</Radio>
-        <Radio value={2}>2</Radio>
-        <Radio value={3}>3</Radio>
-      </RadioGroup>
-      <hr />
-      <Textarea></Textarea>
+      <div></div>
     </ChakraProvider>
   );
 }
